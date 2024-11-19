0a576c4d2e75bd31244a9d35e26612b412a0415a
Denys Vynokurov <denys.vynokurov@gmail.com>
2024-11-19 16:17:24 +0100

    Quartz sync: Nov 19, 2024, 4:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "VCWG Home",
-    titleImage: "/static/my-logo-image.png",
+    titleImage: "/quartz/static/my-logo-image.png",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
