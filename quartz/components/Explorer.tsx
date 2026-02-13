a44aea0fa635c86ad5893657367855b023cdfac0
dvynokur <denys.vynokurov@gmail.com>
2026-02-13 18:58:07 +0100

    Quartz sync: Feb 13, 2026, 6:58 PM

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -17,17 +17,21 @@ const defaultOptions = {
     return node
   },
   sortFn: (a, b) => {
-    const aIsFolder = !a.file
-    const bIsFolder = !b.file
-    // pages first, folders second
-    if (aIsFolder !== bIsFolder) {
-      return aIsFolder ? 1 : -1
+    // Sort order: folders first, then files. Sort folders and files alphabetically
+    if ((!a.file && !b.file) || (a.file && b.file)) {
+      // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+      // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+      return a.displayName.localeCompare(b.displayName, undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
     }
-    // same type → alphabetical (numeric-aware)
-    return a.displayName.localeCompare(b.displayName, undefined, {
-      numeric: true,
-      sensitivity: "base",
-    })
+
+    if (a.file && !b.file) {
+      return 1
+    } else {
+      return -1
+    }
   },
   filterFn: (node) => node.name !== "tags",
   order: ["filter", "map", "sort"],
